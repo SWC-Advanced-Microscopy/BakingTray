530bf21b66530a27fb975b9b36304a757da274f7
Rob Campbell <git@raacampbell.com>
2017-07-14 17:10:18 +0200

    Update preview image once every 10 tiles to improve speed.

--- code/+BakingTray/+gui/acquisition_view.m
+++ code/+BakingTray/+gui/acquisition_view.m
@@ -406,18 +406,22 @@ classdef acquisition_view < BakingTray.gui.child_view
                 return
             end
 
-            %Raise a console warning if it looks like the image has grown in size
-            %TODO: this check can be removed eventually, once we're sure this does not happen ever.
-            if numel(obj.sectionImage.CData) < numel(squeeze(obj.previewImageData(:,:,obj.depthToShow, obj.chanToShow)))
-                fprintf('The preview image data in the acquisition GUI grew in size from %d x %d to %d x %d\n', ...
-                    size(obj.sectionImage.CData,1), size(obj.sectionImage.CData,2), ...
-                    size(obj.previewImageData,1), size(obj.previewImageData,2) )
-            end
+            n=obj.model.currentTilePosition;
+            if n==1 || mod(n,10)==0 || n==length(obj.model.positionArray)
 
-            if obj.rotateSectionImage90degrees
-                obj.sectionImage.CData = rot90(squeeze(obj.previewImageData(:,:,obj.depthToShow, obj.chanToShow)));
-            else
-                obj.sectionImage.CData = squeeze(obj.previewImageData(:,:,obj.depthToShow, obj.chanToShow));
+                %Raise a console warning if it looks like the image has grown in size
+                %TODO: this check can be removed eventually, once we're sure this does not happen ever.
+                if numel(obj.sectionImage.CData) < numel(squeeze(obj.previewImageData(:,:,obj.depthToShow, obj.chanToShow)))
+                    fprintf('The preview image data in the acquisition GUI grew in size from %d x %d to %d x %d\n', ...
+                        size(obj.sectionImage.CData,1), size(obj.sectionImage.CData,2), ...
+                        size(obj.previewImageData,1), size(obj.previewImageData,2) )
+                end
+
+                if obj.rotateSectionImage90degrees
+                    obj.sectionImage.CData = rot90(squeeze(obj.previewImageData(:,:,obj.depthToShow, obj.chanToShow)));
+                else
+                    obj.sectionImage.CData = squeeze(obj.previewImageData(:,:,obj.depthToShow, obj.chanToShow));
+                end
             end
 
         end %updateSectionImage
