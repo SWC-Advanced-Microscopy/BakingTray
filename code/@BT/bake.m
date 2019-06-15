6d3043357f40de95c0cb2324530a4cffd88092fd
Rob Campbell <git@raacampbell.com>
2019-06-15 11:31:11 +0100

    Modify the laser checking routine: see if this helps

--- code/@BT/bake.m
+++ code/@BT/bake.m
@@ -260,9 +260,11 @@ function bake(obj,varargin)
                 pause(3)
                 [isReady,msg]=obj.laser.isReady;
             end
-            if ~obj.laser.isPoweredOn
-                obj.acqLogWriteLine('LASER TURNED OFF: Trying to turn on laser\n');
-                obj.slack('BrainSaw laser turned off. Trying to turn it on again');
+            if ~isReady
+                msg = sprintf('LASER NOT RUNNING: %s\n',msg);
+                obj.acqLogWriteLine(msg);
+                msg = sprintf('%s\n BrainSaw trying to recover it.\n',msg)
+                obj.slack(msg);
                 obj.laser.turnOn
                 pause(3)
                 obj.laser.openShutter
