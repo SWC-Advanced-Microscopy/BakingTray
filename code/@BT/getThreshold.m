b26e64eed9665114e7d55860fbcc85cc23becdd0
Rob Campbell <git@raacampbell.com>
2020-05-01 16:23:21 +0100

    do not try to get threshold if no autoROI stats exist

--- code/@BT/getThreshold.m
+++ code/@BT/getThreshold.m
@@ -12,6 +12,12 @@ function getThreshold(obj)
 
     obj.autoROI.previewImages=obj.returnPreviewStructure;
 
+    % Do not proceed if the image seems empty
+    if all( obj.autoROI.previewImages.imStack(:) == mode(obj.autoROI.previewImages.imStack(:)) )
+        fprintf('BT.%s finds an empty preview image. Not running threshold algorithm\n', ...
+            mfilename)
+        return
+    end
     threshSD = autoROI.autothresh.run(obj.autoROI.previewImages);
 
     obj.autoROI.stats=autoROI(obj.autoROI.previewImages,'tThreshSD',threshSD,'doPlot',false);
