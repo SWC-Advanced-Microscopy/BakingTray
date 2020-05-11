20bd127e13867118aaaf6721ebeaf7268c37977f
Rob Campbell <git@raacampbell.com>
2020-05-11 12:21:27 +0100

    tidy & doc

--- code/+BakingTray/+gui/@acquisition_view/overlayPointsOnImage.m
+++ code/+BakingTray/+gui/@acquisition_view/overlayPointsOnImage.m
@@ -1,14 +1,36 @@
 function H=overlayPointsOnImage(obj,x,y)
-    % Overlay points at defined x and y locations on preview image. This function used for testing right now. 
-    % TESTING
-    % TODO -- tidy and doc if we are to keep this
-    
-    if nargin<2 && ~iscell(x)
-        x = rand(1,200) * 100;
-        y = rand(1,200) * 100;
+    % Plot red circles onto preview image and return handle to the plot object
+    %
+    % function H=acquisition_view.overlayPointsOnImage(x,y)
+    %
+    % Purpose
+    % This method is for test and development purposes. It is used to overlay points
+    % onto the current preview image. It is not called by other methods.
+    % See also overlayTileGridOnImage, and overlayBoundingBoxesOnImage.m
+    %
+    % Inputs
+    % EITHER:
+    % x - scalar or vector of image x axis locations at which to plot.
+    % y - scalar or vector of image y axis locations at which to plot.
+    %
+    % OR:
+    % x - a cell array of matrices where each has two columns, the first is
+    %     y axis position and the second x axis positions. This method
+    %     loops through the cell array, plotting all data.
+    %
+    % Outputs
+    % H - handle to plotted data
+    %
+
+
+    H=[];
+
+    if nargin<2
+        return
     end
 
-    if iscell(x)
+    % Handle case where x is a cell array
+    if nargin==2 && iscell(x)
         B=x;
     else
         B=[];
@@ -16,7 +38,8 @@ function H=overlayPointsOnImage(obj,x,y)
 
     hold(obj.imageAxes,'on')
 
-    if isempty(B)
+    if isempty(B) && nargin==3
+        % The user provided two input arguments
         H=plot(x,y,'or','Parent',obj.imageAxes);
     else
         for ii=1:length(B)
