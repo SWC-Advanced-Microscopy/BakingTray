328b6097f2fc726b17c067862b025a8f2df561b3
Rob Campbell <git@raacampbell.com>
2020-09-17 21:27:33 +0100

    Extract prepare_view.toggleEnable and have it read the stage positions every time it runs

--- code/+BakingTray/+gui/@prepare_view/prepare_view.m
+++ code/+BakingTray/+gui/@prepare_view/prepare_view.m
@@ -502,51 +502,7 @@ classdef prepare_view < BakingTray.gui.child_view
         takeOneSlice(obj,~,~)
         takeNslices(obj,~,~)
         stopAllAxes(obj,~,~)
-
-
-        function toggleEnable(obj,toggleState)
-            % Enables/disables all UI elements. This method is triggered by the callback prepare_view.updateGUIduringAcq so 
-            % that it automatically disables the GUI during preview scans and bakes. 
-            %
-            % Inputs
-            % toggleState - should be the string: 'on' or 'off' The function does nothing if this is not the case
-
-            if ~ischar(toggleState)
-                return
-            end
-
-            if ~strcmpi(toggleState,'on') && ~strcmpi(toggleState,'off')
-                return
-            end
-
-            obj.stopMotion_button.Enable=toggleState;
-            obj.takeSlice_button.Enable=toggleState;
-            obj.takeNSlices_button.Enable=toggleState;
-            obj.setCuttingPos_button.Enable=toggleState;
-            obj.setFrontLeft_button.Enable=toggleState;
-            obj.setVentralMidline_button.Enable=toggleState;
-            obj.moveToSample_button.Enable=toggleState;
-
-            jogButtons=fields(obj.largeStep);
-            for ii=1:length(jogButtons)
-                obj.largeStep.(jogButtons{ii}).Enable=toggleState;
-                obj.smallStep.(jogButtons{ii}).Enable=toggleState;
-            end
-
-            editBoxes=fields(obj.editBox);
-            for ii=1:length(editBoxes)
-                obj.editBox.(editBoxes{ii}).Enable=toggleState;
-            end
-            obj.editBox.cut_Y.Enable='Off'; % At least for now, this is always disabled
-            switch toggleState
-            case 'on'
-                start(obj.prepareViewUpdateTimer);
-            case 'off'
-                stop(obj.prepareViewUpdateTimer);
-            end
-
-        end %toggleEnable
-
+        toggleEnable(obj,toggleState)
     end %Methods
 
 
