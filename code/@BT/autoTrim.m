67f2ab634f27c2a56061f0d62024ee39f1f5bf65
Rob Campbell <git@raacampbell.com>
2020-09-26 22:39:21 +0100

    Add doc text

--- code/@BT/autoTrim.m
+++ code/@BT/autoTrim.m
@@ -1,5 +1,15 @@
 function finished = autoTrim(obj)
     % Trims the block to reach the cutting thickness from the last slice thickness
+    %
+    % hBT.autoTrim
+    %
+    % Purpose
+    % Imaging cut thickness should be gradually approached from thicker sections. This
+    % is to avoid the vibratome from alternately cutting thick and thin slices. This
+    % cuts increasingly thinner slices before taking three slices the target slice 
+    % thickness. The target slice thickness is obtained from the recipe. The cutting
+    % speed is also taken from the recipe.
+
 
     cutSeq = obj.genAutoTrimSequence();
     if isempty(cutSeq)
