e975c13f92f3c39824d10f57768cff9c9fedaced
Rob Campbell <git@raacampbell.com>
2020-11-26 15:23:09 +0000

    An hFastZ setting moves to hStackManager in SI 5.7.1

--- code/components/scanning/@SIBT/SIBT.m
+++ code/components/scanning/@SIBT/SIBT.m
@@ -140,77 +140,6 @@ classdef SIBT < scanner
         end %isReady
 
 
-        function applyZstackSettingsFromRecipe(obj)
-            % applyZstackSettingsFromRecipe
-            % This method is (at least for now) specific to ScanImage. 
-            % Its main purpose is to set the number of planes and distance between planes.
-            % It also sets the the view style to tiled. This method is called by armScanner
-            % but also by external classes at certain times in order to set up the correct 
-            % Z settings in ScanImage so the user can do a quick Grab and check the
-            % illumination correction with depth.
-
-            thisRecipe = obj.parent.recipe;
-            if thisRecipe.mosaic.numOpticalPlanes>1
-                fprintf('Setting up z-scanning with "step" waveform\n')
-
-                % Only change settings that need changing, otherwise it's slow.
-                % The following settings are fixed: they will never change
-                if ~strcmp(obj.hC.hFastZ.waveformType,'step') 
-                    obj.hC.hFastZ.waveformType = 'step'; %Always
-                end
-                if obj.hC.hFastZ.numVolumes ~= 1
-                    obj.hC.hFastZ.numVolumes=1; %Always
-                end
-                if obj.hC.hFastZ.enable ~=1
-                    obj.hC.hFastZ.enable=1;
-                end
-                if obj.hC.hStackManager.stackReturnHome ~= 1
-                    obj.hC.hStackManager.stackReturnHome = 1;
-                end
-
-                % Now set the number of slices and the distance in z over which to image
-                sliceThicknessInUM = thisRecipe.mosaic.sliceThickness*1E3;
-
-
-                if obj.hC.hStackManager.numSlices ~= thisRecipe.mosaic.numOpticalPlanes
-                    obj.hC.hStackManager.numSlices = thisRecipe.mosaic.numOpticalPlanes + thisRecipe.mosaic.numOverlapZPlanes;
-                end
-
-                if obj.hC.hStackManager.stackZStepSize ~= sliceThicknessInUM/thisRecipe.mosaic.numOpticalPlanes;
-                    obj.hC.hStackManager.stackZStepSize = sliceThicknessInUM/thisRecipe.mosaic.numOpticalPlanes;
-                end
-
-
-                if strcmp(obj.hC.hDisplay.volumeDisplayStyle,'3D')
-                    fprintf('Setting volume display style from 3D to Tiled\n')
-                    obj.hC.hDisplay.volumeDisplayStyle='Tiled';
-                end
-
-            else % There is no z-stack being performed
-
-                %Ensure we disable z-scanning if this is not being used
-                obj.hC.hStackManager.numSlices = 1;
-                obj.hC.hStackManager.stackZStepSize = 0;
-                obj.hC.hFastZ.enable=false;
-
-            end
-
-
-            % Apply averaging as needed
-            aveFrames = obj.hC.hDisplay.displayRollingAverageFactor;  
-            if aveFrames>1
-                fprintf('Setting up averaging of %d frames\n', aveFrames)
-            end
-            obj.hC.hScan2D.logAverageFactor = 1; % To avoid warning
-            obj.hC.hStackManager.framesPerSlice = aveFrames;
-            if obj.averageSavedFrames
-                obj.hC.hScan2D.logAverageFactor = aveFrames;
-            else
-                obj.hC.hScan2D.logAverageFactor = 1;
-            end
-
-        end % applyZstackSettingsFromRecipe
-
         function success = disarmScanner(obj)
             if obj.hC.active
                 obj.logMessage(inputname(1),dbstack,7,'Scanner still in acquisition mode. Can not disarm.')
@@ -592,6 +521,7 @@ classdef SIBT < scanner
         scanSettings=returnScanSettings(obj)
         setImageSize(obj,pixelsPerLine,evnt)
         moveFastZTo(obj,targetPositionInMicrons)
+        applyZstackSettingsFromRecipe(obj)
     end %Close SIBT methods in external files
 
 
