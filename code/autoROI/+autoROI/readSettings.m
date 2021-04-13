64d5e0d3f3857b70f727a2960ce6c59c6c393d3c
Rob Campbell <git@raacampbell.com>
2021-04-13 16:00:42 +0100

    move common functions to +autoROI

---
+++ code/autoROI/+autoROI/readSettings.m
@@ -0,0 +1,10 @@
+function settings = readSettings(readFromYaml)
+    % UNFINISHED
+
+    if nargin<1
+        readFromYaml=false;
+    end
+
+
+
+    settings = dynamicThresh_Alg.readSettings(readFromYaml)
