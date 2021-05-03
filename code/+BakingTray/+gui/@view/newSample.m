1dedb6e6caea074d68e2136061db0e0a85b49c47
Rob Campbell <git@raacampbell.com>
2021-05-03 10:48:07 +0100

    Add confirm dialog when starting a new sample

--- code/+BakingTray/+gui/@view/newSample.m
+++ code/+BakingTray/+gui/@view/newSample.m
@@ -1,7 +1,21 @@
 function newSample(obj,~,~)
-    % Loads the default recipe. 
-    % In future could spawn a wizard
+    % Loads the default recipe and resets various parameters in BakingTray.
     
+    % Allow the user to confirm they want to proceed
+    ohYes='Yes!';
+    noWay= 'No way';
+    choice = questdlg('Are you sure you want to start a new sample?', '', ohYes, noWay, noWay);
+
+    switch choice
+        case ohYes
+            % pass
+        case noWay
+            return
+        otherwise
+            return
+    end
+
+
     obj.loadRecipe([],[],fullfile(BakingTray.settings.settingsLocation,'default_recipe.yml'))
     
     % Resonant scanner is turned on if necessary. This gives it the most time possible to warm up
