5363e5b9d57335e54020f724cf0315c4805c6c5c
Rob Campbell <git@raacampbell.com>
2021-05-10 14:41:52 +0100

    bugfix: only reset blade pos if sample is beyond blade. Makes sample surface find hard otherwise.

--- code/+BakingTray/+gui/@prepare_view/resetBladeIfNeeded.m
+++ code/+BakingTray/+gui/@prepare_view/resetBladeIfNeeded.m
@@ -5,18 +5,18 @@ function resetBladeIfNeeded(obj)
     % and it avoids user confusion in cases such as an abort of the cutting 
     % that then leads to the blade returning to a location above the sample
 
-	startPoint = obj.model.recipe.CuttingStartPoint.X; %Cutting start point
-	xPos = obj.model.getXpos;
+    startPoint = obj.model.recipe.CuttingStartPoint.X; %Cutting start point
+    xPos = obj.model.getXpos;
 
     
     if obj.model.recipe.SYSTEM.cutterSide == 1
-    	% Blade to the right as looking at the rig
-    	if xPos > startPoint
+        % Blade to the right as looking at the rig
+        if xPos > startPoint
           obj.model.moveXYto(obj.model.recipe.CuttingStartPoint.X-0.5,0,true)
         end
 
     elseif obj.model.recipe.SYSTEM.cutterSide == -1
-    	if xPos < startPoint
+        if xPos < startPoint
          obj.model.moveXYto(obj.model.recipe.CuttingStartPoint.X+0.5,0,true)
         end
     end
