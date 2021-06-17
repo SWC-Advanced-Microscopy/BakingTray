4036343ec7d538a9ff431dee814d5c09819bd1e5
Rob Campbell <git@raacampbell.com>
2021-06-17 14:06:04 +0100

    Automatically add new settings from default settings file to the user settings file.

--- code/+BakingTray/+settings/settingsLocation.m
+++ code/+BakingTray/+settings/settingsLocation.m
@@ -1,18 +1,20 @@
-function settingsDir=settingsLocation
+function [settingsDir,settingsDirBackup] = settingsLocation
     % Return user settings location of the BakingTray anatomy suite to the command line
     %
-	% function settingsDir=settingsLocation
-	%
-	% Prurpose
+    % function [settingsDir,settingsDirBackup] = settingsLocation
+    %
+    % Prurpose
     % Return user settings directory location of the BakingTray anatomy suite to 
     % the command line. Makes the directory if needed. Returns an empty string on 
     % error.
     % 
-
+    % Outputs
+    % settingsDir - string defining absolute path to BakingTray settings directory. 
+    % settingsDirBackup - string defining absolute path to BakingTray backup settings directory.
 
     installDir = BakingTray.settings.installLocation;
     if isempty(installDir)
-    	settingsDir=[];
+        settingsDir=[];
         return
     end
 
@@ -23,6 +25,19 @@ function settingsDir=settingsLocation
         success=mkdir(settingsDir);
         if ~success
             fprintf('FAILED TO MAKE SETTINGS DIRECTORY: %s. Check the permissions and try again\n', settingsDir);
+            return
+        end
+    end
+
+
+    % Same for the backup settings dir
+    settingsDirBackup = fullfile(installDir,'SETTINGS_BACKUP');
+
+    %Make the settings directory if needed
+    if ~exist(settingsDirBackup,'dir')
+        success=mkdir(settingsDirBackup);
+        if ~success
+            fprintf('FAILED TO MAKE BACKUP SETTINGS DIRECTORY: %s. Check the permissions and try again\n', settingsDirBackup);
             return
         end
     end
