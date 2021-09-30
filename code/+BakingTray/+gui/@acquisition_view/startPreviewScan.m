b49638302b93a5e139c28b8327b183dcb07b7a7d
Rob Campbell <git@raacampbell.com>
2021-09-30 11:10:53 +0100

    Do not overlay stage boundaries on image when preview scan finished.

--- code/+BakingTray/+gui/@acquisition_view/startPreviewScan.m
+++ code/+BakingTray/+gui/@acquisition_view/startPreviewScan.m
@@ -48,8 +48,6 @@ function startPreviewScan(obj,~,~)
     obj.button_BakeStop.Enable='on'; 
     obj.depthSelectPopup.Enable=depthEnableState; %return to original state
 
-
-    obj.overlayStageBoundariesOnImage
     % Run auto-ROI stuff (the following only runs if the recipe says we are in auto-ROI mode)
     obj.overlayThreshBorderOnImage
 
